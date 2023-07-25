7ee3bf3b1dc3ee6c381a5c02ceaef2fd470ba132
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-07-25 11:31:21 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -63,7 +63,7 @@ async def save_group(bot, message):
                                                                          [[
                                                                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                                                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                                                                      """  ],[
+                                                                          ] """[
                                                                            InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorrio")"""
                                                                          ]]
                                                  ),
