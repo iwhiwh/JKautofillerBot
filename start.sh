c8b33ce3a14cf0aee17cb3e57ba41071ad7cf3d1
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-07-26 21:05:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Apnamovi/Automatic.git /Automatic
+  git clone https://github.com/JKautofillerBot/Automatic.git /Automatic
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Automatic
