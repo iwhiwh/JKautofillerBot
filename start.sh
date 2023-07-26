3e7dd15c81e0a84814c0ad4a7a16415c29ab8428
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-07-26 21:20:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
+
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/JKautofillerBot/Automatic.git /Automatic
+  git clone https://github.com/iwhiwh/JKautofillerBot.git /JKautofillerBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Automatic
+  git clone $UPSTREAM_REPO /JKautofillerBot
 fi
 cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
